bde94d8287639cba292b939840f5a00a611acbcf
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-04-14 01:55:48 +0700

    Update delete.sh

--- ssh/delete.sh
+++ ssh/delete.sh
@@ -14,3 +14,7 @@ echo -e "===============================" | lolcat
 echo -e "     Script By MakhlukVpn  "
 echo -e "===============================" | lolcat
 fi
+
+read -n 1 -s -r -p "Press [ Enter ] to back on menu"
+
+menu
