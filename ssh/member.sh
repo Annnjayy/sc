534590de8ebe30d1fedb625bd8ab5c71e50be7db
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-04-14 01:57:55 +0700

    Update member.sh

--- ssh/member.sh
+++ ssh/member.sh
@@ -28,5 +28,7 @@ echo -e "===============================" | lolcat
 echo -e "     Script By MakhlukVpn          "
 echo -e "===============================" | lolcat
 echo -e ""
-sleep 4
+
+read -n 1 -s -r -p "Press [ Enter ] to back on menu"
+
 menu
