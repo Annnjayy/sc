acdd78d07a659b221dacbff9ca1d5a9a9ad5f27a
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-04-19 06:35:05 +0700

    Update menu-cf.sh

--- menu/menu-cf.sh
+++ menu/menu-cf.sh
@@ -33,8 +33,8 @@ function lane() {
 }
 
 function LOGO() {
-	echo -e ""
-	echo -e "===============================" | lolcat
+    echo -e ""
+    echo -e "===============================" | lolcat
     echo -e "       ⇱ Menu Slowdns ⇲"
     echo -e "===============================" | lolcat
 	echo -e ""
@@ -104,47 +104,19 @@ function add_cert(){
     systemctl enable xray@vless-tls
     systemctl enable xray@vless-nontls
     systemctl enable xray@trojan
-    echo -e "[ ${green}INFO${NC} ] Processing 10 second to stop port 80 " 
-sleep 2
-echo -e "1"
-sudo fuser -k 80/tcp
-sleep 0.2
-echo -e "2"
-sudo fuser -k 80/tcp
-sleep 0.2
-echo -e "3"
-sudo fuser -k 80/tcp
-sleep 0.2
-echo -e "4"
-sudo fuser -k 80/tcp
-sleep 0.2
-echo -e "5"
-sudo fuser -k 80/tcp
-sleep 0.2
-echo -e "6"
-sudo fuser -k 80/tcp
-sleep 0.2
-echo -e "7"
-sudo fuser -k 80/tcp
-sleep 0.2
-echo -e "8"
-sudo fuser -k 80/tcp
-sleep 0.2
-echo -e "9"
-sudo fuser -k 80/tcp
-sleep 0.2
-echo -e "10"
-sudo fuser -k 80/tcp
-sleep 0.2
+    systemctl stop nginx
     echo -e "[ ${green}INFO${NC} ] Starting renew cert... " 
     sleep 2
+    /root/.acme.sh/acme.sh --upgrade --auto-upgrade
+    /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
     /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-    ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+    ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /usr/local/etc/xray/xray.crt --keypath /usr/local/etc/xray/xray.key --ecc
     systemctl restart xray@v2ray-tls
     systemctl restart xray@v2ray-nontls
     systemctl restart xray@vless-tls
     systemctl restart xray@vless-nontls
     systemctl restart xray@trojan
+    systemctl restart nginx
     echo -e "[ ${green}INFO${NC} ] All finished... " 
     sleep 0.5
 }
