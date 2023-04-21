e955015f332ae29456cf28ffeb8ea7ee42422221
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-04-21 11:04:13 +0700

    Add files via upload

--- menu/limitspeed.sh
+++ menu/limitspeed.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-clear
-#color
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Loading..."
-IZIN=$( curl https://raw.githubusercontent.com/Annnjayy/sc/main/name | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "[ ${green}INFO${NC} ] Permission Accepted..."
-else
-echo -e "[ ${green}INFO${red} ] Permission Denied!${NC}";
-echo -e "[ ${green}INFO${NC} ] Please Contact Admin!!"
-echo -e "[ ${green}INFO${NC} ] WhatsApp : 087844547312"
-echo -e "[ ${green}INFO${NC} ] Telegram : https://t.me/MakhlukVpn"
-exit 0
-fi
-clear
+
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[ON]${Font_color_suffix}"
 Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
@@ -35,6 +18,8 @@ wondershaper -a $NIC -d $down -u $up > /dev/null 2>&1
 systemctl enable --now wondershaper.service
 echo "start" > /home/limit
 echo "Done"
+echo "Reboot Vps...."
+sleep 0.5
 fi
 }
 function stop () {
@@ -74,4 +59,3 @@ clear
 echo " You Entered The Wrong Number"
 menu
 fi
-
